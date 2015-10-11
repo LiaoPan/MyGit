f731d2e48d3f0cb6b29791fc48b4471ae6a2afc1
lcylp <1920283113@qq.com>
2015-10-11 17:04:01 +0800

    删除mysql数据库数据

---
+++ truncate.py
@@ -0,0 +1,11 @@
+import mysql.connector
+class Truncate():
+    def truncate(self):
+        db=mysql.connector.connect(user='root',password='lcy492',database='spiderdb',charset='utf8')
+        cursor=db.cursor()
+        sql='TRUNCATE spiderdb.security'
+        cursor.execute(sql)
+        db.commit()
+        db.close()
+t=Truncate()
+t.truncate()
